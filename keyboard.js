adad49ed368509a8e732cd3bb311aae40c01ce8a
aswan aswan <aaswan048@gmail.com>
2023-01-05 12:39:30 +0000

    last3 commit

--- keyboard.js
+++ keyboard.js
@@ -1,20 +1,6 @@
 let paragraph = "korem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 let starter = false ;
 
-let tx = fetch("https://baconipsum.com/?paras=5&type=all-meat&start-with-lorem=1", {
-    "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-language": "en-US,en;q=0.9,ar;q=0.8",
-    },
-    "method": "GET",
-    'mode': 'no-cors',
-});
-tx.then(res => res.text()).then(data => console.log(data));
-
-
-
-
-
 
 let firsttext = paragraph.split(" ").slice(0 , 40).join(" ") ;
 let content = "" ;
