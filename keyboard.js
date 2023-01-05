56659b2d2ccad593dd562cfd806600bfe81d7d56
aswan aswan <aaswan048@gmail.com>
2023-01-05 11:14:41 +0000

    third commit

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,21 @@
 let paragraph = "korem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 let starter = false ;
 
+let tx = fetch("https://baconipsum.com/?paras=5&type=all-meat&start-with-lorem=1", {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "en-US,en;q=0.9,ar;q=0.8",
+    },
+    "method": "GET",
+    'mode': 'no-cors',
+});
+tx.then(res => res.text()).then(data => console.log(data));
+
+
+
+
+
+
 let firsttext = paragraph.split(" ").slice(0 , 40).join(" ") ;
 let content = "" ;
 [...firsttext].forEach((ele , index) => {
@@ -26,7 +41,6 @@ function change(){
 
 function myStopFunction() {
     clearInterval(start);
-    // console.log('stoped') ;
 }
 
 
@@ -118,25 +132,22 @@ document.body.addEventListener("keypress" , (e)=> {
     if(starter == "done" ){
         document.querySelector(".header").classList.add("done") ;
         // stop the game 
-        let all = document.querySelectorAll(".char")
+        let all = document.querySelectorAll(".char") ;
         all.forEach(ele =>{
             ele.classList.remove("char") ;
         })
-        // count words
-        let x = document.querySelector(".words")
-        let y = document.querySelector(".mytext").textContent.split(" ")
-        let s = document.querySelector('.seconds')
-        x.textContent =  y.filter( ele => ele.length >= 4  ).length + " in " + (60 - s.textContent.replace("s" , ""))+"s";
+
+        // how many words  in seconds 
+        let s = document.querySelector('.seconds') ;
+        let x_container = document.querySelector(".words") ;
+        let w = document.querySelectorAll(".verified").length ;
+        x_container.textContent = Math.floor(w/4)  + " in " + (60 - s.textContent.replace("s" , ""))+"s" ;
+
+        // how many mistakes
         let m  = document.querySelectorAll(".mistake").length ;
         let ms = document.querySelector(".mistakes") ;
         ms.textContent = m ;
     }
-
-
-        
-
-
-
 
 })
 
